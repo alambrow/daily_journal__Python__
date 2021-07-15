61d87c0118912f863e258008bed2504acb6e3364
alambrow <alexander.lambrow@gmail.com>
2021-07-15 15:32:15 -0500

    foreign key now corresponds correctly

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -1,3 +1,6 @@
+DROP TABLE Entries;
+DROP TABLE Moods;
+
 CREATE TABLE `Entries` (
     `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     `title`  TEXT NOT NULL,
@@ -7,11 +10,13 @@ CREATE TABLE `Entries` (
 
 CREATE TABLE `Moods` (
     `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `mood`  TEXT NOT NULL
+    `mood`  TEXT NOT NULL,
+    `entry_id` INTEGER NOT NULL,
+    FOREIGN KEY(`entry_id`) REFERENCES `Entries`(`id`)
 );
 
 INSERT INTO 'Entries' VALUES (null, 'Learning SQL', 'Learning about basic commands', 'July 15 2021');
-INSERT INTO 'Moods' VALUES (null, 'trying to keep up the pace');
+INSERT INTO 'Moods' VALUES (null, 'trying to keep up the pace', 1);
 
 SELECT * FROM Entries;
 SELECT * FROM Moods;
